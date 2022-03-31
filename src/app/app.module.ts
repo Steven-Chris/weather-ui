3cfd70f45df6f3a2652cb47292f6d1fd30598fcb
steven.chris <steven.chris_entvickler.com#EXT#@DoctorsGroup237.onmicrosoft.com>
2022-03-31 13:49:00 +0530

    API intergration done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WeatherCardComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
